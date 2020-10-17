dcde60fd8d0989ed8cc509aeb0189b1925826f7b
anil <webanilsidhu@gmail.com>
2020-10-18 03:45:48 +0530

    order table

---
+++ database/migrations/2020_10_17_220918_create_orders_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->integer('order_id');
+            $table->integer('user_id');
+            $table->string('address');
+            $table->string('status');
+            $table->string('payment_status');
+            $table->string('payment_method');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
